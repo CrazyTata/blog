c4dec383ec7a05eb81feca1fd34c9a5d8125fb1d
CrazyTata <1027532238@qq.com>
2018-12-08 16:11:06 +0800

    add hui

--- public/js/index/index.js
+++ public/js/index/index.js
@@ -1,1 +1,22 @@
-index.js
+(function(){
+	var model = {
+		initial:function(){
+			model.global();
+            model.bind();
+		},
+		global:function(){
+			model.data = new Vue({
+				el:'#index',
+				data:{
+
+				},
+				watch:{
+
+				},
+				methods:{
+
+				},
+			})
+		}
+	}
+})();
